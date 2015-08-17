2ead0c4a79209b4004afcc4bcdcc8bca2cb83b3d
Alex Snow <a.shulhin@gmail.com>
2015-08-17 20:51:34 +0200

    Initial commit

---
+++ common/config/params.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'adminEmail' => 'ash@bigmir.net',
+    'supportEmail' => 'ash@bigmir.net',
+    'user.passwordResetTokenExpire' => 3600,
+];
---
+++ console/config/params.php
@@ -0,0 +1,4 @@
+<?php
+return [
+    'adminEmail' => 'ash@bigmir.net',
+];
